e9a006a6a1fd7979d6a259d930413539f5857deb
ohcnivek <64056046+ohcnivek@users.noreply.github.com>
2022-01-12 14:54:43 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './assets/seinfeld.GIF';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          lol
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
